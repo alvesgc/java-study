6b8d7244c1c84e5bf657a1d40466177989731664
Alisson <alissonalves64@hotmail.com>
2023-08-20 18:53:32 -0300

    feat: initial config

---
+++ simplifyPpay/src/main/java/com/simplifyPpay/SimplifyPpayApplication.java
@@ -0,0 +1,13 @@
+package com.simplifyPpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifyPpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifyPpayApplication.class, args);
+	}
+
+}
---
+++ simplifyPpay/src/test/java/com/simplifyPpay/SimplifyPpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplifyPpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifyPpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

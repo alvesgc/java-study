89c421e00f768a9d429117db34ed22ba9f0658ca
Alisson <alissonalves64@hotmail.com>
2023-08-20 19:24:39 -0300

    feat: create validation

--- simplifyPpay/src/main/java/com/simplifyPpay/repositories/UserRepository.java
+++ simplifyPpay/src/main/java/com/simplifyPpay/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
  Optional<User> findUserByDocument(String document);
  Optional<User> findUserByDocument(Long id);
+
+ Optional<Object> findUserById(Long id);
 }
